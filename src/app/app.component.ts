da561e7a9ef2c95789a4181b47e17094d9f5da96
Przemek Urbaniak <thekyez@gmail.com>
2022-12-04 01:11:58 +0100

    jakos dziala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rafal';
+  name = 'Angular ' + VERSION.major;
+
+  persons: string[] = [
+
+  ];
+
+  pairs: any[] = []; //[  ['kasia', 'tomek'] ]
+  result: string = '';
+  index = 0;
+
+  form = new FormGroup({
+    name: new FormControl()
+  })
+
+
+  constructor() {
+    let fetchedData = JSON.parse(localStorage.getItem('persons')!);
+    console.log(fetchedData);
+    if(fetchedData && fetchedData.length) {
+      this.persons = [...fetchedData];
+      this.prepareData();
+    }
+  }
+
+  save() {
+    localStorage.setItem('persons', JSON.stringify(this.persons));
+  }
+
+  clear() {
+    localStorage.removeItem('persons');
+    this.persons = [];
+    this.prepareData();
+  }
+
+  addPerson() {
+    this.persons.push(this.form.controls['name'].value);
+
+    this.form.reset();
+    this.prepareData();
+  }
+
+  shuffleArray(array: any[]) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
+
+  prepareData() {
+    this.pairs = [];
+    this.shuffleArray(this.persons);
+
+    console.log(this.persons);
+
+    this.persons.forEach((person) => {
+      this.pairs.push([person, undefined]);
+    });
+
+    console.log(this.pairs);
+  }
+
+  randomPresent() {
+
+    this.prepareData();
+
+
+    this.persons.forEach((person) => {
+      const emptyPairs = this.pairs.filter((tab) => {
+        return tab[1] === undefined && tab[0] !== person;
+      });
+
+      const randomizedPerson =
+        emptyPairs[Math.floor(Math.random() * emptyPairs.length)];
+
+      const findPairIndex = this.pairs.findIndex((tab) => {
+        return tab[0] === randomizedPerson[0];
+      });
+
+      const findPair = this.pairs.find((tab) => {
+        return tab[0] === randomizedPerson[0];
+      });
+
+      this.pairs[findPairIndex] = [findPair![0], person];
+    });
+
+    console.log(this.pairs);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
